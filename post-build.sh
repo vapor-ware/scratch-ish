b247dbe4270ceab0698f9b9e98fed1994d28b1c6
Erick Daniszewski <edaniszewski@gmail.com>
2020-07-21 12:23:46 -0400

    ...

--- post-build.sh
+++ post-build.sh
@@ -17,12 +17,12 @@ if [[ ! -f "${base_path}/exists" ]]; then
 fi
 
 # Create a temporary directory and move the existing binary into it.
-mkdir -p ${base_path}/tmp
-mv ${base_path}/exists ${base_path}/tmp/exists
+mkdir -p "${base_path}/tmp"
+mv "${base_path}/exists" "${base_path}/tmp/exists"
 
 # Compress the binary with UPX (https://github.com/upx/upx). This cuts ~1MB from
 # the curiously large binary. (1.5MB -> ~450kB)
-docker run --rm -w $PWD -v $PWD:$PWD gruebel/upx:latest --best --lzma -o ${base_path}/exists ${base_path}/tmp/exists
+docker run --rm -w $PWD -v $PWD:$PWD gruebel/upx:latest --best --lzma -o "${base_path}/exists" "${base_path}/tmp/exists"
 
 # Remove temporary files.
-rm -rf ${base_path}/tmp
+rm -rf "${base_path}/tmp"
